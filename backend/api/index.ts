c52455532c947ed7f64ac3bcc49599af85e6eb6c
Adeboye Oluwatobi <tobiadeboye23@gmail.com>
2026-06-07 17:40:29 +0100

    Initialize invitations in hosted backend

--- backend/api/index.ts
+++ backend/api/index.ts
@@ -1,8 +1,16 @@
 import { app } from "../src/app.js";
 import { ensureAppraisalWorkflowColumns } from "../src/services/appraisalService.js";
+import { ensurePasswordResetTokensTable, ensureUserRoleConstraint } from "../src/services/authService.js";
+import { ensureInvitationInfrastructure } from "../src/services/invitationService.js";
 import { ensureReviewPeriodsTable } from "../src/services/reviewPeriodService.js";
 
-const initialization = Promise.all([ensureReviewPeriodsTable(), ensureAppraisalWorkflowColumns()]);
+const initialization = Promise.all([
+  ensureReviewPeriodsTable(),
+  ensureUserRoleConstraint(),
+  ensurePasswordResetTokensTable(),
+  ensureInvitationInfrastructure(),
+  ensureAppraisalWorkflowColumns()
+]);
 
 export default async function handler(
   req: Parameters<typeof app>[0],
